386d2afd45c6fb578da7ebe7efd5f6dfb97ebd4a
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-02-21 17:16:04 -0300

    11. Using Types

--- Project-setup/index.js
+++ Project-setup/index.js
@@ -1,1 +1,7 @@
-console.log("Code here!");
+function sumNumbers(x, y) {
+    return x + y;
+}
+var numberX = 5;
+var numberY = 2.5;
+var sumResult = sumNumbers(numberX, numberY);
+console.log(sumResult);
