100f38c5e4bdc6f9b80e2d6d0acdbdbbe1e14fd8
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-02-21 17:16:52 -0300

    11. Using Types

---
+++ 11/index.ts
@@ -0,0 +1,10 @@
+function sumNumbers(x: number, y: number) {
+  return x + y;
+}
+
+const numberX = 5;
+const numberY = 2.5;
+
+const sumResult = sumNumbers(numberX, numberY);
+
+console.log(sumResult);
