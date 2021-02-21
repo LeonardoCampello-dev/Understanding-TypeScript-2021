7088298f06d8b8f34658b4352800f0b547e81dd0
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-02-21 17:35:46 -0300

    12. TypeScript Types vs JavaScript Types

---
+++ 12/index.ts
@@ -0,0 +1,17 @@
+function sumNumbers(x: number, y: number) {
+  // Bad implementation, we can solve this easily with TypeScript.
+  // if (typeof x !== "number" || typeof y !== "number") {
+  //   throw new Error("Incorrect input!");
+  // } 
+  
+  return x + y;
+}
+
+const numberX = 5;
+const numberY = 2.5;
+
+const sumResult = sumNumbers(numberX, numberY);
+
+console.log(sumResult);
+
+// TypeScript verification errors only at development time.
