9c3fe7a96d9b997fda0d3f0d1ba3603467db21ae
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2021-02-23 08:40:25 -0300

    19. Working with Tuples

---
+++ 19/index.ts
@@ -0,0 +1,13 @@
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Leonardo",
+  age: 20,
+  hobbies: ["music", "games", "sports"],
+  role: [1, "developer"],
+};
+
+console.log(person);
---
+++ Project-setup/index.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "Leonardo",
+    age: 20,
+    hobbies: ["music", "games", "sports"],
+    role: [1, "developer"]
+};
+console.log(person);
