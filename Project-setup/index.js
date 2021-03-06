5d6fc30fd929cb73c3a636ec7e8d55bfd1b10169
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2021-03-06 17:52:16 -0300

    23. Literal Types

--- Project-setup/index.js
+++ Project-setup/index.js
@@ -1,14 +1,17 @@
-function combine(firstInput, secondInput) {
+function combine(firstInput, secondInput, resultConvertion) {
     var result;
-    if (typeof firstInput === "number" && typeof secondInput === "number") {
-        result = firstInput + secondInput;
+    if ((typeof firstInput === "number" && typeof secondInput === "number") ||
+        resultConvertion === "as-number") {
+        result = +firstInput + +secondInput;
     }
     else {
         result = firstInput.toString() + secondInput.toString();
     }
     return result;
 }
-var combinedAges = combine(20, 21);
+var combinedAges = combine(20, 21, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Leonardo", "Bianca");
+var combinedStringAges = combine("20", "21", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Leonardo", "Bianca", "as-text");
 console.log(combinedNames);
